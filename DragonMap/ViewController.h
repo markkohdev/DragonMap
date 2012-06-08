f53618576e209e0835538379394e0a8b8273454d
Admin <Admin@Macintosh-001b631efbf9-2.local>
2012-06-08 14:32:23 -0400

    Initial Commit

---
+++ DragonMap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragonMap
+//
+//  Created by Youngmoo Kim on 6/8/12.
+//  Copyright (c) 2012 Drexel University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DragonMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DragonMap
+//
+//  Created by Youngmoo Kim on 6/8/12.
+//  Copyright (c) 2012 Drexel University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
